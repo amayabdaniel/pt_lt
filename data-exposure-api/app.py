1c4ade9b70424379d2efbbee9964c42106c26dbd
amayabdaniel <daniel.amaya.buitrago@outlook.com>
2024-10-04 22:46:22 -0500

    9

--- data-exposure-api/app.py
+++ data-exposure-api/app.py
@@ -1,18 +1,21 @@
 from fastapi import FastAPI, HTTPException
 from google.cloud import bigquery
 import os
+import uvicorn
 
 app = FastAPI()
 
+# Configurar variables de entorno
 PROJECT_ID = os.getenv("PROJECT_ID")
 BIGQUERY_DATASET = os.getenv("BIGQUERY_DATASET")
 BIGQUERY_TABLE = os.getenv("BIGQUERY_TABLE")
 
+# Inicializar el cliente de BigQuery
 client = bigquery.Client(project=PROJECT_ID)
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the Data Exposure API"}
+    return {"message": "Bienvenido a la API de Exposición de Datos"}
 
 @app.get("/data")
 def get_data():
@@ -25,10 +28,9 @@ def get_data():
     results = query_job.result()
     data = [dict(row) for row in results]
     if not data:
-        raise HTTPException(status_code=404, detail="No data found.")
+        raise HTTPException(status_code=404, detail="No se encontraron datos.")
     return {"data": data}
 
 if __name__ == "__main__":
-    import uvicorn
     port = int(os.getenv("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
